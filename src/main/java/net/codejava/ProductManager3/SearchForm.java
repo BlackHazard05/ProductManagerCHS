508b61b3ad74a785262591e927fbaf6da52acc06
Cesar Hdez <hazard_95_@outlook.com>
2023-01-27 00:11:09 -0600

    Version 2 Product Manager

---
+++ src/main/java/net/codejava/ProductManager3/SearchForm.java
@@ -0,0 +1,15 @@
+package net.codejava.ProductManager3;
+
+public class SearchForm {
+	String text;
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	
+}
